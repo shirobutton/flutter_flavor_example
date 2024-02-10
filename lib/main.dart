c40860eb8d42474ad085177d8c9bea37f324f8a3
shirobutton <shirobutton@gmail.com>
2024-02-10 17:56:21 +0900

    main関数にFirebase用のコードを追加

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,28 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  await FirebaseMessaging.instance.requestPermission(
+    alert: true,
+    announcement: false,
+    badge: true,
+    carPlay: false,
+    criticalAlert: false,
+    provisional: false,
+    sound: true,
+  );
+
+  final fcmToken = await FirebaseMessaging.instance.getToken();
+  print(fcmToken);
+
+  FirebaseMessaging.onMessage.listen((RemoteMessage message) {
+    print(
+        'Message title: ${message.notification?.title}, body: ${message.notification?.body} data: ${message.data}');
+  });
+
   runApp(const MyApp());
 }
 
